168ce0b956be8c7e6da7dbc820ef70db3d61f467
Freddy <freddyvelarde59@gmail.com>
2022-02-08 17:17:05 -0400

    some changes

--- src/database.js
+++ src/database.js
@@ -1,6 +1,7 @@
 const mongoose  = require('mongoose');
 
-const URI = 'mongodb://localhost:27017/notes_db';
+// const URI = 'mongodb://localhost:27017/notes_db';
+const URI = `mongodb+srv://${process.env.USER_DB}:${process.env.PASSWD_DB}@cluster0.n4yjh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
 const config = {
   useNewUrlParser: true,
   useUnifiedTopology: true
